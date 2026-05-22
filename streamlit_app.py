f11c4eba9a3ab7c3357a5d1bfac5b38a4be75ecd
rachma85 <120188967+rachma85@users.noreply.github.com>
2026-05-22 10:34:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 Aplikasi Rachma")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+action = st.menu_button("Export", options=["nama", "asal", "umur"])
+if action == "nama":
+    st.write("Kiki")
+elif action == "asal":
+    st.write("Bogor")
+elif action == "umur":
+    st.write("20 tahun")
